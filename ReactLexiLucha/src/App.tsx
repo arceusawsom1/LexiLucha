5bfe08a0a8255ae30d28d6d71052bc48c685e22d
Ryan Wilson <Ryan.Wilson1@fdmgroup.com>
2023-11-06 17:11:09 +1100

    Added Alert Snackboxes

--- ReactLexiLucha/src/App.tsx
+++ ReactLexiLucha/src/App.tsx
@@ -1,16 +1,48 @@
 import './App.css'
-import { Container, ThemeProvider, createTheme } from '@mui/material'
+import { Container, ThemeProvider, createTheme, Snackbar, Alert } from '@mui/material'
 import BasicPhraseQuestion from './components/BasicPhraseQuestion'
+import { useEffect,useState } from 'react';
 
 function App() {
   const theme = createTheme({
     
   })
+  const [successMessage, setSuccessMessage] = useState("");
+  const [successOpen, setSuccessOpen] = useState(false);
+  const [failMessage, setFailMessage] = useState("");
+  const [failOpen, setFailOpen] = useState(false);
+  useEffect(()=>{
+    if (successMessage!=""){
+      setSuccessOpen(true);
+      handleFailClose()
+    }
+  },[successMessage])
+  useEffect(()=>{
+    if (failMessage!=""){
+      setFailOpen(true);
+      handleSuccessClose();
+    }
+  },[failMessage])
+  const handleSuccessClose = () => {
+    setSuccessOpen(false);
+    setSuccessMessage("");
+  }
+  const handleFailClose = () =>{
+    setFailOpen(false);
+    setFailMessage("");
+  }
+
   return (
     <ThemeProvider theme = {theme}>
       <Container>
-        <BasicPhraseQuestion />
+        <BasicPhraseQuestion correctHandler={setSuccessMessage} failHandler={setFailMessage}/>
       </Container>
+      <Snackbar anchorOrigin={{vertical:"bottom",horizontal:"center"}} autoHideDuration={2000} open={successOpen} onClose={handleSuccessClose}>
+        <Alert severity="success">{successMessage}</Alert>
+      </Snackbar>
+      <Snackbar anchorOrigin={{vertical:"bottom",horizontal:"center"}} autoHideDuration={2000} open={failOpen}  onClose={handleFailClose}>
+        <Alert severity="error">{failMessage}</Alert>
+      </Snackbar>
     </ThemeProvider>
   )
 }
