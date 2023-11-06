48efa1d70c6ded113c8be7c42baf3177bb41d16f
Ryan Wilson <Ryan.Wilson1@fdmgroup.com>
2023-11-06 17:10:24 +1100

    Created a basic card component for showing some stats to the user

---
+++ ReactLexiLucha/src/components/StatView.tsx
@@ -0,0 +1,21 @@
+import { Card, Container, Grid, Typography } from "@mui/material";
+import { IStats } from "./BasicPhraseQuestion";
+
+interface IProps {
+    stats: IStats
+}
+const StatView = ({stats}:IProps) => {
+
+    return(
+        <Grid container direction={"column"} alignItems={"center"}>
+            <Grid item xs={3}>
+                <Card sx={{mt:2,width:"200px", textAlign:"left",pl:2,py:1}}>
+                    <Typography variant="h4">Stats</Typography>
+                    <Typography>Streak: {stats.streak}</Typography>
+                    <Typography>Total Correct: {stats.correct}</Typography>
+                </Card>
+            </Grid>
+        </Grid>
+    )
+}
+export default StatView;
