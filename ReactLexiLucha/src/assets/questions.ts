378d4c0eb667f06435c5c8ace0d63026c95a52e1
Ryan Wilson <Ryan.Wilson1@fdmgroup.com>
2023-11-06 17:09:20 +1100

    Added a number of basic dummy questions for local testing purposes

---
+++ ReactLexiLucha/src/assets/questions.ts
@@ -0,0 +1,26 @@
+import createQuestion from "../utils.tsx/QuestionCreator";
+
+const questions = [
+    createQuestion("¿Dónde está la biblioteca?", "Where is the library?", "I need to study."),
+    createQuestion("Me gusta bailar salsa", "I like to dance salsa", "It's a fun activity."),
+    createQuestion("¿Cuándo es tu cumpleaños?", "When is your birthday?", "I want to celebrate with you."),
+    createQuestion("Ella habla francés muy bien", "She speaks French very well", "It's impressive."),
+    createQuestion("¿Cuántos años tienes?", "How old are you?", "I'm just curious."),
+    createQuestion("Voy al supermercado a comprar comida", "I'm going to the supermarket to buy food", "Do tomorrow you need anything?"),
+    createQuestion("¿Qué hora es?", "What time is it?", "I don't want to be late."),
+    createQuestion("Mi perro es muy juguetón", "My dog is very playful", "He keeps me active."),
+    createQuestion("¿Has estado en España?", "Have you been to Spain?", "I love traveling."),
+    createQuestion("Nos vemos mañana en el parque", "See you tomorrow at the park", "I'll bring some snacks."),
+    createQuestion("¿Cómo te llamas?", "What's your name?", "I'd like to get to know you."),
+    createQuestion("Estoy aprendiendo a cocinar", "I'm learning to cook", "It's a useful skill."),
+    createQuestion("¿Qué te gusta hacer en tu tiempo libre?", "What do you like to do in your free time?", "I'm looking for a new hobby."),
+    createQuestion("Hace buen tiempo hoy", "The weather is nice today", "Let's go outside."),
+    createQuestion("¿Cuál es tu película favorita?", "What's your favorite movie?", "I need a good recommendation."),
+    createQuestion("Mi hermana estudia medicina", "My sister is studying medicine", "It's a challenging field."),
+    createQuestion("¿Dónde vives?", "Where do you live?", "I might be nearby."),
+    createQuestion("Me encanta la música clásica", "I love classical music", "It's so soothing."),
+    createQuestion("¿Tienes alguna mascota?", "Do you have any pets?", "I adore animals."),
+    createQuestion("Voy a visitar a mis abuelos este fin de semana", "I'm going to visit my grandparents this weekend", "Family is important to me."),
+
+]
+export default questions;
