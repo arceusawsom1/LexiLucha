58e116c793f2ed38260c0ec128e1d9d0bdd9d017
Ryan Wilson <ryan.wilson1@fdmgroup.com>
2023-11-08 11:33:32 +1100

    Adding settings file for constants

---
+++ ReactLexiLucha/src/assets/settings.ts
@@ -0,0 +1,6 @@
+// The delay after a word is submitted via the smart input before new characters are recognized
+// The intention is to stop trailing letters showing up at the start of the next input.
+const SMART_WORD_INPUT_DELAY = 300
+
+
+export {SMART_WORD_INPUT_DELAY}
