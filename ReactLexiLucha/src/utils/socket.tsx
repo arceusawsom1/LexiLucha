18c672db9a95c699ea788feb691b68b580e4c760
Ryan <ryanwilson99@outlook.com>
2023-11-14 11:20:58 +1100

    Changed handshake path on react

--- ReactLexiLucha/src/utils/socket.tsx
+++ ReactLexiLucha/src/utils/socket.tsx
@@ -3,4 +3,6 @@ import { io } from 'socket.io-client';
 const URL = import.meta.env.VITE_SOCKET_BUILD; 
 
 console.log(URL)
-export const socket = io(URL);
+export const socket = io(URL,{
+    path:"/api/socket/socket.io"  //Change this to only be used in prod
+});
