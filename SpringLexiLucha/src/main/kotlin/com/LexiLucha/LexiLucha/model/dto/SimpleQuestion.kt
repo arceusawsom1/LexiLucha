2c71e2acb74ff6f0b2c917156d12eeda7b2b1ad4
Ryan <ryanwilson99@outlook.com>
2023-11-15 14:50:46 +1100

    Added the space back

--- SpringLexiLucha/src/main/kotlin/com/LexiLucha/LexiLucha/model/dto/SimpleQuestion.kt
+++ SpringLexiLucha/src/main/kotlin/com/LexiLucha/LexiLucha/model/dto/SimpleQuestion.kt
@@ -3,5 +3,5 @@ package com.LexiLucha.LexiLucha.model.dto
 import com.LexiLucha.LexiLucha.model.Question
 
 data class SimpleQuestion(val id: Int, val phrase: String, val options: String){
-    constructor(question: Question) : this(question.id, question.phrase, question.answer + question.noiseWords)
+    constructor(question: Question) : this(question.id, question.phrase, question.answer + " " + question.noiseWords)
 }
