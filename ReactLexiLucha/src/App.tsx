3da8af1606a260c0a30ec3b3a357060c23be2899
Ryan Wilson <ryan.wilson1@fdmgroup.com>
2023-11-17 10:58:27 +1100

    Created empty dashboard page

--- ReactLexiLucha/src/App.tsx
+++ ReactLexiLucha/src/App.tsx
@@ -1,12 +1,27 @@
-import { Route, Routes } from 'react-router-dom'
+import { Link, Route, Routes } from 'react-router-dom'
 import './App.css'
 import MainGamePage from './pages/MainGamePage'
+import { Box, Button, Container } from '@mui/material'
+import Dashboard from './pages/Dashboard'
 
 function App() {
+    
   return (
-    <Routes>
-        <Route path="" Component={MainGamePage}/>
-    </Routes>
+    <>
+        <Routes>
+            <Route path="" Component={MainGamePage}/>
+            <Route path="dashboard" Component={Dashboard}/>
+        </Routes>
+        <Container sx={{textAlign:"center",my:4}}>
+            <Box sx={{my:1}}>
+                <Button component={Link} variant="outlined" to="/">New Game</Button>
+            </Box>
+            <Box sx={{my:1}}>
+                <Button component={Link} variant="outlined" to="dashboard">Dashboard</Button>
+            </Box>
+
+        </Container>
+    </>
   )
 }
 
