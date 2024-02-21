28b680b8c5220b43cee20f7ede53e3d01255e9ef
Ryan <ryanwilson99@outlook.com>
2024-02-21 23:43:20 +1100

    fixed syntax error

--- SpringLexiLucha/src/main/kotlin/com/LexiLucha/LexiLucha/dal/QuestionRepository.kt
+++ SpringLexiLucha/src/main/kotlin/com/LexiLucha/LexiLucha/dal/QuestionRepository.kt
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.Query
 
 interface QuestionRepository : JpaRepository<Question, Int> {
 
-    @Query("SELECT q.id FROM Question q WHERE q.language=:language && q.mode=:mode")
+    @Query("SELECT q.id FROM Question q WHERE q.language=:language AND q.mode=:mode")
     fun findIdsByLanguageAndMode(language: LANGUAGE, mode: QUESTIONMODE): List<Int>
 
     @Query("SELECT DISTINCT q.mode FROM Question q WHERE q.language=:language")
